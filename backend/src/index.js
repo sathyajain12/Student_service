c4a15c16887a85077b038e1a4a8a24816fb7e68f
Sri Sai Sathya Jain <saisathyajain@sssihl.edu.in>
2026-02-10 15:49:14 +0530

    fixed delete bug

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,7 +27,7 @@ export default {
         // CORS Headers
         const corsHeaders = {
             'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+            'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
             'Access-Control-Allow-Headers': 'Content-Type, Authorization',
         };
 
