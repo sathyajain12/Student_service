79f49dc368dc6fdd5ffa66767a5912cb1f84df51
Sri Sai Sathya Jain <saisathyajain@sssihl.edu.in>
2026-03-03 12:17:31 +0530

    updated digilocker instructions

--- frontend/src/formConfigs.js
+++ frontend/src/formConfigs.js
@@ -95,7 +95,7 @@ export const FORM_CONFIGS = {
     'duplicate-grade-card': {
         needsDirectorApproval: true,
         title: 'Application for Duplicate Grade Card',
-        description: 'This form is applicable only to students belonging to batches up to and including 2020. Students from 2021 batch onwards can download their Duplicate Grade Cards from [DigiLocker](https://accounts.digilocker.gov.in/v3/7b9f84c86732efd21cd8076ff06f3fd60b1fbe146732fa57444b03b35f3740a4--en).',
+        description: 'This form is applicable only to those students who joined the Institute in 2020 or earlier. Students who joined from 2021 onwards can download their Duplicate Grade Cards from [DigiLocker](https://accounts.digilocker.gov.in/v3/7b9f84c86732efd21cd8076ff06f3fd60b1fbe146732fa57444b03b35f3740a4--en).',
         paymentApplicationName: 'SSSIHL Exams App Duplicate Grade Card',
         instructions: [
             'Before applying for a Duplicate Grade Card, the applicant must make all reasonable efforts to trace the Original Grade Card. Only after being fully satisfied that the Original Grade Card has been lost beyond recovery may the candidate proceed to apply for a Duplicate Grade Card.',
@@ -267,7 +267,7 @@ export const FORM_CONFIGS = {
     },
     'duplicate-degree': {
         title: 'Application for Duplicate Degree Certificate',
-        description: 'This form is applicable only to students belonging to batches up to and including 2020. Students from 2021 batch onwards can download their Duplicate Degree Certificate from [DigiLocker](https://accounts.digilocker.gov.in/v3/7b9f84c86732efd21cd8076ff06f3fd60b1fbe146732fa57444b03b35f3740a4--en).',
+        description: 'This form is applicable only to those students who joined the Institute in 2020 or earlier. Students who joined from 2021 onwards can download their Duplicate Degree Certificate from [DigiLocker](https://accounts.digilocker.gov.in/v3/7b9f84c86732efd21cd8076ff06f3fd60b1fbe146732fa57444b03b35f3740a4--en).',
         instructions: [
             'Before applying for a Duplicate Degree Certificate, the applicant must make all reasonable efforts to trace the Original Degree Certificate. Only after being fully satisfied that the Original Degree Certificate has been lost beyond recovery may the candidate proceed to apply for a Duplicate Degree Certificate.',
             'In the event of loss in transit or misplacement of the Original Degree Certificate, the candidate is required to lodge a police complaint and obtain an official acknowledgment from the concerned police station. A scanned copy of the police complaint, along with the original acknowledgment, must be uploaded.',
